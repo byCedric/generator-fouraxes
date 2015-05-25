b182a74df69301ea3f62ff7d8e6c168758bd50da
Cedric van Putten <cedric@Cedrics-MacBook-Air.local>
2015-05-25 13:01:00 +0200

    Added all file assertions within the app tests.

--- test/test-app.js
+++ test/test-app.js
@@ -9,16 +9,58 @@ describe('fouraxes:app', function () {
 	before(function (done) {
 		helpers.run(path.join(__dirname, '../generators/app'))
 			.withOptions({ skipInstall: true })
-			.withPrompts({ someOption: true })
+			.withPrompts({ appName: 'Test App', useGitkeeps: true })
 			.on('end', done);
 	});
 
-	it('creates files', function () {
+	it('creates project files', function () {
 		assert.file([
+			'.editorconfig',
+			'.jshintrc',
+			'.jscsrc',
+			'.bowerrc',
+			'.gitignore',
 			'bower.json',
 			'package.json',
-			'.editorconfig',
-			'.jshintrc'
+			'Gruntfile.js',
+			'requirejs.js'
+		]);
+	});
+
+	it('creates grunt files', function () {
+		assert.file([
+			'tasks/babel.js',
+			'tasks/clean.js',
+			'tasks/connect.js',
+			'tasks/copy.js',
+			'tasks/jscs.js',
+			'tasks/jshint.js',
+			'tasks/requirejs.js',
+			'tasks/sass.js',
+			'tasks/watch.js'
+		]);
+	});
+
+	it('creates app files', function () {
+		assert.file([
+			'build/index.html',
+			'app/index.html',
+			'app/styles/main.scss',
+			'app/scripts/main.js',
+			'app/scripts/model.js',
+			'app/scripts/view.js',
+			'app/scripts/collection.js',
+			'app/scripts/router.js'
+		]);
+	});
+
+	it('creates gitkeep files', function () {
+		assert.file([
+			'app/scripts/models/.gitkeep',
+			'app/scripts/views/.gitkeep',
+			'app/scripts/collections/.gitkeep',
+			'app/scripts/routers/.gitkeep',
+			'app/scripts/templates/.gitkeep'
 		]);
 	});
 });
